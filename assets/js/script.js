73cdad4a040f08c5590183ee177f5f7a02671a56
carmelopalumbo <carmelopalumbo@outlook.it>
2022-11-11 15:18:40 +0100

    add-get-api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,31 @@
 // vue
+
+const {createApp} = Vue;
+
+createApp({
+
+    data(){
+        return{
+            mailList: [],
+            apiUrl: 'https://flynn.boolean.careers/exercises/api/random/mail',
+            count: 0
+        }
+    },
+
+    methods: {
+        getApi(){
+            if(this.count < 10){
+                axios.get(this.apiUrl)
+                .then( result => {
+                    this.mailList.push(result.data.response);
+                })
+                return this.getApi(++this.count)
+            }
+            console.log(this.isLoad);
+        }
+    },
+
+    mounted(){
+        this.getApi();
+    }
+}).mount('#app')
